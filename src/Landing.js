a8d86d0a0e8d20a399a123f421cc90b72fa6bb82
Himank Goel <himankgoel03@gmail.com>
2018-10-20 11:24:11 +0530

    Minor Changes

--- src/Landing.js
+++ src/Landing.js
@@ -4,20 +4,18 @@ import './App.css';
 class Landing extends Component {
 
   constructor(props) {
-    super(props)
-    this.uploadFile = this.uploadFile.bind(this);
+    super(props);
+    this.state = {
+      acceptedFiles: [],
+      rejectedFiles: []
+    };
   }
 
-  uploadFile(event) {
-    let file = event.target.files[0];
-    console.log(file);
-
-    if (file) {
-      let data = new FormData();
-      data.append('file', file);
-      console.log(data);
-      // axios.post('/files', data)...
-    }
+  onDrop(acceptedFiles, rejectedFiles) {
+    this.setState({
+      acceptedFiles,
+      rejectedFiles
+    });
   }
 
   render() {
@@ -31,20 +29,99 @@ class Landing extends Component {
             <div className="title">
               Convert your docs into stunning presentations
             </div>
-            <span>
-              <input type="file"
-                name="myFile"
-                onChange={this.uploadFile} />
-            </span>
+            <div className="upload--file">
+              <span>
+                <Dropzone
+                  onDrop={this.onDrop.bind(this)}
+                  accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                  disableClick
+                  className="drop-zone"
+                >
+                  {({ open }) => (
+                    <React.Fragment>
+                      <p>
+                        Drag a file here or{" "}
+                        <button
+                          className="browse--file"
+                          type="button"
+                          onClick={() => open()}
+                        >
+                          browse
+                        </button>{" "}
+                        for a file to upload
+                      </p>
+                    </React.Fragment>
+                  )}
+                </Dropzone>
+              </span>
+              <div className="choosen-file">
+                {this.state.acceptedFiles.map(f => (
+                  <span key={f.name}>
+                    Choosen File - <span className="file--name">{f.name}</span>
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="alt-opt">
+              <span>or</span>
+            </div>
           </div>
         </div>
-        <style jsx>{`
-          .navbar {
-            font-size: 3.5em;
-            padding: 25px;
-            font-family: 'Righteous', cursive;
-          }
-        `}
+        <style jsx>
+          {`
+            .navbar {
+              font-size: 3.5em;
+              padding: 25px;
+              font-family: "Righteous", cursive;
+            }
+            .content {
+              display: flex;
+              flex-direction: column;
+            }
+            .title,
+            .alt-opt {
+              font-size: 25px;
+              color: #b9b9b9;
+            }
+            .drop-zone {
+              padding: 40px;
+              min-height: 13vh;
+              width: 90vh;
+              margin: 0 auto;
+              margin-top: 60px;
+              border: 1px dashed #ffffff;
+              border-radius: 15px;
+              position: relative;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              opacity: 0.8;
+            }
+            .browse--file {
+              background: none;
+              border: none;
+              box-shadow: none;
+              color: #7b10ff;
+              font-family: Montserrat, "sans-serif";
+              font-size: 16px;
+              margin: 0;
+              padding: 0;
+              text-decoration: underline;
+            }
+            .browse--file:focus {
+                outline: none;
+            }
+            .choosen-file {
+                margin-top: 25px;
+                font-size: 20px;
+            }
+            .file--name {
+                color: #8872fc;
+            }
+            .alt-opt {
+                margin-top: 25px;
+            }
+          `}
         </style>
       </div>
     );
